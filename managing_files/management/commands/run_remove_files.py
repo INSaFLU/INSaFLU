c3ab62c3276fde362fc0a443c7cfb0c78a755f1a
mmp <mpinheiro@ophiomics.com>
2018-11-11 15:55:36 +0000

    [add] improve remove files on file system

--- managing_files/management/commands/run_remove_files.py
+++ managing_files/management/commands/run_remove_files.py
@@ -5,8 +5,9 @@ Created on Jan 5, 2018
 '''
 from django.core.management import BaseCommand
 from managing_files.models import Reference, Sample, Project, ProjectSample, UploadFiles
-from constants.constants import TypePath, TypeFile, FileType
+from constants.constants import TypePath, TypeFile
 from constants.software_names import SoftwareNames
+from django.contrib.auth.models import User
 from utils.utils import Utils
 from utils.software import Software
 import datetime, dateutil.relativedelta
@@ -40,10 +41,16 @@ class Command(BaseCommand):
 	def add_arguments(self, parser):
 		parser.add_argument('--only_identify_files', type=str2bool, nargs='?',
 			const=True, default=True, 
-			help='Only to identify files to remove. Does not remove them. The output it is in log file')
+			help='Only to identify files to remove. Does not remove them. The output it is in log file.')
 		parser.add_argument('--remove_files_after_days', type=int, nargs='?',
-			const=True, default=10, 
+			const=10, default=10, 
 			help='Remove files after X days passed from been removed in web site by the users.')
+		parser.add_argument('--login_to_pass', type=str, nargs='?',
+			const="", default="",
+			help='Login accounts to pass the remove files. Can be more than one separated by comma. Ex: mmp,demo')
+		parser.add_argument('--delete_original_fastq', type=str2bool, nargs='?',
+			const=False, default=False,
+			help='Delete fastq original files if True. It is possible to remove these files because the system only works Trimmomatic output files.')
 
 
 	# A command must define handle()
@@ -54,22 +61,34 @@ class Command(BaseCommand):
 		
 		### if true does not remove files, only save it in the log file
 		only_identify_files = options['only_identify_files']
-		self.out_message("\nMode - {}".format("Only identify files" if only_identify_files else "Identify files and remove them."), False)
+		delete_original_fastq = options['delete_original_fastq']
+		self.out_message("\nMode - {}\nDelete Original fastq files:{}\nRemove After Days:{}".format(
+			"Only identify files" if only_identify_files else "Identify files and remove them.",
+			delete_original_fastq, self.REMOVE_FILES_AFTER_DAYS), False)
+		
+		## logins to pass
+		logins_to_pass = options['login_to_pass']
+		lst_accounts_to_pass = []
+		if (len(logins_to_pass) > 0): lst_accounts_to_pass = logins_to_pass.strip().split(',')
+		
+		## test the existence of accounts
+		self.test_existence_accounts(lst_accounts_to_pass)
 		
 		### delete REFERENCES
-		total_files_downloaded = self.delete_references(only_identify_files)
+		total_files_downloaded = self.delete_references(only_identify_files, lst_accounts_to_pass)
 
 		### delete SAMPLES
-		total_files_downloaded += self.delete_all_samples(only_identify_files)
+		total_files_downloaded += self.delete_all_samples(only_identify_files, lst_accounts_to_pass)
 		
 		### delete original fastq SAMPLES
-		total_files_downloaded += self.delete_original_fastq_samples(only_identify_files)
+		if (delete_original_fastq):
+			total_files_downloaded += self.delete_original_fastq_samples(only_identify_files, lst_accounts_to_pass)
 
 		### remove fastq.gz files not attached and already deleted
-		total_files_downloaded += self.delete_upload_deleted_files_by_batch_and_not_attached(only_identify_files)
+		total_files_downloaded += self.delete_upload_deleted_files_by_batch_and_not_attached(only_identify_files, lst_accounts_to_pass)
 		
 		### remove project_samples "sample" is the sample and project sample was deleteds
-		total_files_downloaded += self.delete_project_samples(only_identify_files)
+		total_files_downloaded += self.delete_project_samples(only_identify_files, lst_accounts_to_pass)
 
 		if (only_identify_files):
 			if (total_files_downloaded == 0): self.out_message("None references were identified", False)
@@ -83,9 +102,29 @@ class Command(BaseCommand):
 		else: self.logger_remove_files.info(message.strip())
 		self.stdout.write(message)
 
+	def test_existence_accounts(self, lst_accounts_to_pass):
+		if (len(lst_accounts_to_pass) > 0):
+			self.out_message("\nTesting existence of the accounts - {}".format(",".join(lst_accounts_to_pass)), False)
+			lst_not_exist = []
+			for account in lst_accounts_to_pass:
+				try:
+					User.objects.all().get(username=account)
+				except User.DoesNotExist:
+					lst_not_exist.append(account)
+			
+			### testing accounts not exist
+			if (len(lst_not_exist) > 0):
+				for account in lst_not_exist:
+					self.out_message("\nAccount does not exist: {}".format(account), True)
+				sys.exit(1)
+			self.out_message("\nAccounts OK", False)
+				
 
-	def delete_references(self, only_identify_files):
+
+	def delete_references(self, only_identify_files, lst_accounts_to_pass):
 		"""
+		param: only_identify_files
+		param: lst_accounts_to_pass
 		delete file references deleted by the users 
 		"""
 		### REFERENCES - removing references files
@@ -93,6 +132,8 @@ class Command(BaseCommand):
 		references = Reference.objects.all().filter(is_deleted=True, is_deleted_in_file_system=False)
 		count = 0
 		for reference in references:
+			### test the owners
+			if (reference.owner.username in lst_accounts_to_pass): continue
 			if (Project.objects.all().filter(reference=reference, is_deleted=False).count() == 0):
 				files_removed = []
 				files_to_remove = []
@@ -137,7 +178,7 @@ class Command(BaseCommand):
 		self.out_message("### END  REFERENCES\n", False)
 		return count
 
-	def delete_all_samples(self, only_identify_files):
+	def delete_all_samples(self, only_identify_files, lst_accounts_to_pass):
 		"""
 		delete all samples deleted by the users 
 		"""
@@ -145,7 +186,7 @@ class Command(BaseCommand):
 		samples_deleted = 0
 		try:
 			samples = Sample.objects.all().filter(is_deleted=True, is_deleted_in_file_system=False, is_valid_1=True, is_ready_for_projects=True)
-			samples_deleted = self._delete_all_samples(samples, only_identify_files)
+			samples_deleted = self._delete_all_samples(samples, only_identify_files, lst_accounts_to_pass)
 		except Sample.DoesNotExist:
 			pass
 		
@@ -153,7 +194,7 @@ class Command(BaseCommand):
 		date_remove = datetime.datetime.now() - dateutil.relativedelta.relativedelta(months=1)
 		samples = Sample.objects.all().filter(is_deleted=True, is_deleted_in_file_system=False, is_valid_1=True, 
 								is_ready_for_projects=False, creation_date__lte=date_remove)
-		samples_deleted += self._delete_all_samples(samples, only_identify_files)
+		samples_deleted += self._delete_all_samples(samples, only_identify_files, lst_accounts_to_pass)
 		
 		if (samples_deleted == 0):
 			if (only_identify_files): self.out_message("None samples were identified", False)
@@ -161,12 +202,13 @@ class Command(BaseCommand):
 		self.out_message("### END  Samples\n", False)
 		return samples_deleted
 
-	def _delete_all_samples(self, samples, only_identify_files):
+	def _delete_all_samples(self, samples, only_identify_files, lst_accounts_to_pass):
 		"""
 		Delete all samples by array
 		"""
 		count = 0
 		for sample in samples:
+			if (sample.owner.username in lst_accounts_to_pass): continue
 			if (ProjectSample.objects.all().filter(sample=sample, is_deleted=False).count() == 0):
 				files_removed = []
 				files_to_remove = []
@@ -210,10 +252,11 @@ class Command(BaseCommand):
 								upload_files = UploadFiles.objects.all().filter(samples__id=sample.pk, 
 											type_file__name=TypeFile.TYPE_FILE_fastq_gz, is_deleted_in_file_system=False)
 								for upload_file in upload_files:
-									upload_file.is_delete = True
+									upload_file.is_deleted = True
 									if (upload_file.date_deleted == None):
 										upload_file.date_deleted = datetime.datetime.now()
-									upload_file.is_deleted_in_file_system = True
+									## don't set this flag were, the next method will remove the files in "upload_file"
+									#upload_file.is_deleted_in_file_system = True
 									upload_file.save()
 							
 				except Exception as e:
@@ -234,13 +277,13 @@ class Command(BaseCommand):
 		return count
 
 
-	def delete_original_fastq_samples(self, only_identify_files):
+	def delete_original_fastq_samples(self, only_identify_files, lst_accounts_to_pass):
 		"""
 		delete all samples deleted by the users 
 		"""
 		self.out_message("\n### Original fastq files\n", False)
 		samples = Sample.objects.all().filter(is_deleted=False, is_deleted_in_file_system=False, is_valid_1=True, is_ready_for_projects=True)
-		samples_deleted = self._delete_original_fastq(samples, only_identify_files)
+		samples_deleted = self._delete_original_fastq(samples, only_identify_files, lst_accounts_to_pass)
 		
 		if (samples_deleted == 0):
 			if (only_identify_files): self.out_message("None original fastq.gz were identified", False)
@@ -248,7 +291,7 @@ class Command(BaseCommand):
 		self.out_message("### END  Original fastq files\n", False)
 		return samples_deleted
 
-	def _delete_original_fastq(self, samples, only_identify_files):
+	def _delete_original_fastq(self, samples, only_identify_files, lst_accounts_to_pass):
 		"""
 		Delete all samples by array
 		"""
@@ -258,6 +301,7 @@ class Command(BaseCommand):
 			files_to_remove = []
 			
 			if (sample.is_original_fastq_removed()): continue
+			if (sample.owner.username in lst_accounts_to_pass): continue
 			
 			## can be removed already
 			try:
@@ -285,10 +329,11 @@ class Command(BaseCommand):
 						upload_files = UploadFiles.objects.all().filter(samples__id=sample.pk, 
 									type_file__name=TypeFile.TYPE_FILE_fastq_gz, is_deleted_in_file_system=False)
 						for upload_file in upload_files:
-							upload_file.is_delete = True
+							upload_file.is_deleted = True
 							if (upload_file.date_deleted == None):
 								upload_file.date_deleted = datetime.datetime.now()
-							upload_file.is_deleted_in_file_system = True
+							## don't set this flag were, the next method will remove the files in "upload_file"
+							#upload_file.is_deleted_in_file_system = True
 							upload_file.save()
 							
 			except Exception as e:
@@ -309,7 +354,7 @@ class Command(BaseCommand):
 		return count
 
 
-	def delete_upload_deleted_files_by_batch_and_not_attached(self, only_identify_files):
+	def delete_upload_deleted_files_by_batch_and_not_attached(self, only_identify_files, lst_accounts_to_pass):
 		"""
 		delete deleted files that are uploaded by batch
 		"""
@@ -320,6 +365,9 @@ class Command(BaseCommand):
 		for upload_file in upload_files:
 			files_removed = []
 			files_to_remove = []
+			
+			### test the owner
+			if (upload_file.owner.username in lst_accounts_to_pass): continue
 			
 			## can be removed already
 			try:
@@ -361,8 +409,9 @@ class Command(BaseCommand):
 			else: self.out_message("Files removed: {}".format(count), False)
 		self.out_message("### END Uploaded fastq files by batch\n", False)
 		return count
+	
 
-	def delete_project_samples(self, only_identify_files):
+	def delete_project_samples(self, only_identify_files, lst_accounts_to_pass):
 		"""
 		delete project samples if samples was deleted
 		"""
@@ -376,6 +425,9 @@ class Command(BaseCommand):
 			files_removed = []
 			files_to_remove = []
 
+			## test the owner
+			if (project_sample.project.owner.username in lst_accounts_to_pass): continue
+			
 			## can be removed already
 			try:
 				### files from snippy

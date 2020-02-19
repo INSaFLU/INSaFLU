4d6535e33d2d89cbfb89d1d1ba44436b010c70fc
Miguel Pinheiro <monsanto@ua.pt>
2020-02-19 10:04:16 +0000

    [add] db port number by .env file

--- fluwebvirus/settings.py
+++ fluwebvirus/settings.py
@@ -190,7 +190,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': '5432',
+        'PORT': config('DB_PORT', default='5432'),
         'TEST': {
             'NAME': 'fluwebvirus_test',
         },

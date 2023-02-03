42e23476cb36f332196b6fa381428dbc8f82a6f0
douradojns <dourado.jns@gmail.com>
2023-02-03 11:03:05 +0000

    decode system deploty stdout if bytes

--- pathogen_identification/modules/object_classes.py
+++ pathogen_identification/modules/object_classes.py
@@ -237,6 +237,9 @@ class RunCMD:
         )
         out, err = proc_prep.communicate()
 
+        if isinstance(out, bytes):
+            out = out.decode("utf-8", errors="ignore")
+
         exec_time = time.perf_counter() - start_time
 
         return out, err, exec_time

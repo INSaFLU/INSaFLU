4ff8ada9610d46ff64e72b9e6c5963412771544d
SantosJGND <dourado.jns@gmail.com>
2023-06-02 15:35:58 +0000

    gather intermediate reports. in runmain.

--- pathogen_identification/models.py
+++ pathogen_identification/models.py
@@ -8,7 +8,7 @@ from managing_files.models import Sample
 from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
 from django import forms
-
+#from pathogen_identification.modules.object_classes import IntermediateFiles
 # Create your models here.
 
 # Create your models here.
@@ -523,6 +523,26 @@ class RunMain(models.Model):
     def sorted_reports_get(self):
 
         return FinalReport.objects.filter(run=self).order_by('-coverage')
+    
+    def intermediate_reports_get(self):
+            
+        contig_classification = ContigClassification.objects.get(
+                    run= self
+                )
+        read_classification = ReadClassification.objects.get(
+                    run= self   
+                )
+        run_remap = RunRemapMain.objects.get(run=self)
+
+        intermediate_reports = [
+            read_classification.read_classification_report,
+            contig_classification.contig_classification_report,
+            run_remap.merged_log,
+            run_remap.remap_plan,
+        ]
+
+        return intermediate_reports
+
 
     def delete_data(self):
 
--- pathogen_identification/modules/object_classes.py
+++ pathogen_identification/modules/object_classes.py
@@ -1369,3 +1369,14 @@ class Assembly_results:
     assembly_mean: int
     assembly_max: int
     assembly_trim: int
+
+@dataclass(frozen=True)
+class IntermediateFiles:
+    read_classification_report: str
+    contig_classification_report: str
+    remap_main_report: str
+    database_matches: str
+
+    @property
+    def files(self):
+        return [self.read_classification_report, self.contig_classification_report, self.remap_main_report, self.database_matches]

e201e8da00917f7c488186b62b5ddfb09e2b9534
SantosJGND <dourado.jns@gmail.com>
2023-06-06 10:23:19 +0000

    merge with param remap

--- pathogen_identification/utilities/televir_parameters.py
+++ pathogen_identification/utilities/televir_parameters.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 from constants.software_names import SoftwareNames
+from pathogen_identification.constants_settings import ConstantsSettings as CS
 from pathogen_identification.models import Projects
 from settings.models import Parameter, Software
 
@@ -124,3 +125,11 @@ class TelevirParameters:
         )
 
         return prinseq
+
+    @staticmethod
+    def get_read_overlap_threshold():
+        """
+        Get overlap threshold
+        """
+
+        return CS.READ_OVERLAP_THRESHOLD

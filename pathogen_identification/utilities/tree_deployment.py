58b0479838124b2b121a640178de2dfa999061d8
SantosJGND <dourado.jns@gmail.com>
2023-07-08 18:35:00 +0000

    update trees graph setup trees function to include multiple trees

--- pathogen_identification/utilities/tree_deployment.py
+++ pathogen_identification/utilities/tree_deployment.py
@@ -12,32 +12,21 @@ import pandas as pd
 from constants.constants import Televir_Metadata_Constants as Televir_Metadata
 from constants.constants import TypePath
 from fluwebvirus.settings import STATIC_ROOT
-from pathogen_identification.constants_settings import ConstantsSettings as PIConstants
-from pathogen_identification.models import (
-    FinalReport,
-    ParameterSet,
-    PIProject_Sample,
-    Projects,
-    RunMain,
-    SoftwareTree,
-    SoftwareTreeNode,
-)
+from pathogen_identification.constants_settings import \
+    ConstantsSettings as PIConstants
+from pathogen_identification.models import (FinalReport, ParameterSet,
+                                            PIProject_Sample, Projects,
+                                            RunMain, SoftwareTree,
+                                            SoftwareTreeNode)
 from pathogen_identification.modules.remap_class import Mapping_Instance
 from pathogen_identification.modules.run_main import RunMainTree_class
-from pathogen_identification.utilities.televir_parameters import TelevirParameters
+from pathogen_identification.utilities.televir_parameters import \
+    TelevirParameters
 from pathogen_identification.utilities.update_DBs_tree import (
-    Update_Assembly,
-    Update_Classification,
-    Update_Remap,
-    Update_RunMain_Initial,
-    Update_RunMain_Secondary,
-    get_run_parents,
-)
+    Update_Assembly, Update_Classification, Update_Remap,
+    Update_RunMain_Initial, Update_RunMain_Secondary, get_run_parents)
 from pathogen_identification.utilities.utilities_pipeline import (
-    Pipeline_Makeup,
-    PipelineTree,
-    Utils_Manager,
-)
+    Pipeline_Makeup, PipelineTree, Utils_Manager)
 from pathogen_identification.utilities.utilities_views import ReportSorter
 from settings.constants_settings import ConstantsSettings
 from utils.utils import Utils
@@ -1370,11 +1359,11 @@ class TreeProgressGraph:
 
     def get__combined_progress_df(self):
         ## setup a deployment and record the progress
-        # module_tree = self.setup_combined_tree()
-        # deployment_tree = Tree_Progress(module_tree, self.sample, self.project)
+        module_tree = self.setup_combined_tree()
+        deployment_tree = Tree_Progress(module_tree, self.sample, self.project)
 
-        # stacked_df = deployment_tree.stacked_changes_log()
-        stacked_df = self.setup_trees()
+        stacked_df = deployment_tree.stacked_changes_log()
+        #stacked_df = self.setup_trees()
         stacked_df.to_csv(self.stacked_df_path, sep="\t")
 
         return stacked_df

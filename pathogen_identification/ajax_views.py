c23241a02271837963e677baff035618457b0558
santosjgnd <joaos3092@gmail.com>
2023-07-11 13:58:22 +0000

    check sorting necessary prior to deploy.

--- pathogen_identification/ajax_views.py
+++ pathogen_identification/ajax_views.py
@@ -7,7 +7,8 @@ from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.http import require_POST
-
+from pathogen_identification.utilities.televir_parameters import TelevirParameters
+from pathogen_identification.utilities.utilities_views import ReportSorter
 from constants.meta_key_and_values import MetaKeyAndValue
 from fluwebvirus.settings import STATIC_ROOT, STATIC_URL
 from managing_files.models import ProcessControler
@@ -266,16 +267,26 @@ def sort_report_projects(request):
         process_SGE = ProcessSGE()
         user = request.user
 
-        sample_id = int(request.POST["sample_id"])
+        sample_id = int(request.POST["pisample_id"])
         sample = PIProject_Sample.objects.get(id=int(sample_id))
 
-        try:
-            taskID = process_SGE.set_submit_televir_sort_pisample_reports(
-                user=request.user,
-                pisample_pk=sample.pk,
-            )
+        final_reports = FinalReport.objects.filter(sample=sample)
+        report_layout_params = TelevirParameters.get_report_layout_params(
+            project_pk=sample.project.pk
+        )
+        report_sorter = ReportSorter(final_reports, report_layout_params)
 
-            data["is_deployed"] = True
+        try:
+            if report_sorter.run is None:
+                pass
+            elif report_sorter.check_analyzed():
+                pass
+            else:
+                taskID = process_SGE.set_submit_televir_sort_pisample_reports(
+                    user=request.user,
+                    pisample_pk=sample.pk,
+                )
+                data["is_deployed"] = True
 
         except Exception as e:
             print(e)

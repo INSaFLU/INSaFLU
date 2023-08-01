140cdbb1b4a29909a875fde737a8e57dd73af1c3
SantosJGND <dourado.jns@gmail.com>
2023-08-01 11:29:38 +0000

    remove set controls from view

--- pathogen_identification/views.py
+++ pathogen_identification/views.py
@@ -662,9 +662,6 @@ class MainPage(LoginRequiredMixin, generic.CreateView):
             self.request, paginate={"per_page": Constants.PAGINATE_NUMBER}
         ).configure(samples)
 
-        ### set control reports
-        set_control_reports(project.pk)
-
         ### type of deployment
         DEPLOY_TYPE= PICS.DEPLOYMENT_DEFAULT
         DEPLOY_URL= 'deploy_ProjectPI'

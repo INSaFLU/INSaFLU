b9b631b59122aa1ef3268ebc9fbf59718a52f0c4
SantosJGND <dourado.jns@gmail.com>
2023-08-28 12:55:34 +0000

    ommit generate_default_trees

--- pathogen_identification/management/commands/generate_default_trees.py
+++ pathogen_identification/management/commands/generate_default_trees.py
@@ -20,7 +20,7 @@ class Command(BaseCommand):
         default_software.test_all_defaults_pathogen_identification(user_system)
 
         utils = Utils_Manager()
-        utils.generate_default_trees(user_system)
+        #utils.generate_default_trees(user_system)
 
         default_software.remove_all_parameters(user_system)
 

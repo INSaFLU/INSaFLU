6260f4ba49edba1068a902ff9ff3c1b4f2b35bbc
SantosJGND <dourado.jns@gmail.com>
2023-09-18 16:01:54 +0000

    skip description query in metagenomics, retrieve file directly

--- pathogen_identification/modules/metadata_handler.py
+++ pathogen_identification/modules/metadata_handler.py
@@ -170,6 +170,7 @@ class Metadata_handler:
     def generate_targets_from_report(self, df: pd.DataFrame):
         references_table = self.filter_references_table(df)
         # references_table = references_table.drop_duplicates(subset=["taxid"])
+        references_table.rename(columns={"accid": "acc"}, inplace=True)
         references_table = self.merge_report_to_metadata_taxid(references_table)
 
         remap_targets, _ = self.generate_mapping_targets(

4a1c698c2b8400d73ebbb6fa15a37dc48a840be9
SantosJGND <dourado.jns@gmail.com>
2023-11-22 16:41:48 +0000

    work on final report

--- pathogen_identification/templatetags/report_colors.py
+++ pathogen_identification/templatetags/report_colors.py
@@ -91,7 +91,7 @@ def map_success_col(success_count):
 def depth_color_error(depth_value, max_value):
     print(depth_value, max_value)
     if depth_value and max_value:
-        ncol = (1 - float(depth_value)) * 100 / float(max_value)
+        ncol = float(depth_value) * 100 / float(max_value)
     else:
         ncol = 0
 

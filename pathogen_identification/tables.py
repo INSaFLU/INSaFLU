c20862c8a09b8ccb0199be79fc5d175f44f7e18c
SantosJGND <dourado.jns@gmail.com>
2023-12-19 16:44:55 +0000

    adjust counts runmain televir

--- pathogen_identification/tables.py
+++ pathogen_identification/tables.py
@@ -11,20 +11,32 @@ from constants.constants import Constants
 from managing_files.manage_database import ManageDatabase
 from managing_files.models import ProcessControler
 from pathogen_identification.constants_settings import ConstantsSettings as CS
-from pathogen_identification.models import (ContigClassification, FinalReport,
-                                            ParameterSet, PIProject_Sample,
-                                            Projects, RawReference,
-                                            ReadClassification,
-                                            ReferenceContigs, RunAssembly,
-                                            RunDetail, RunMain, SampleQC,
-                                            TelevirRunQC)
-from pathogen_identification.utilities.televir_parameters import \
-    TelevirParameters
+from pathogen_identification.models import (
+    ContigClassification,
+    FinalReport,
+    ParameterSet,
+    PIProject_Sample,
+    Projects,
+    RawReference,
+    ReadClassification,
+    ReferenceContigs,
+    RunAssembly,
+    RunDetail,
+    RunMain,
+    SampleQC,
+    TelevirRunQC,
+)
+from pathogen_identification.utilities.televir_parameters import TelevirParameters
 from pathogen_identification.utilities.utilities_general import (
-    get_project_dir, get_project_dir_no_media_root)
+    get_project_dir,
+    get_project_dir_no_media_root,
+)
 from pathogen_identification.utilities.utilities_views import (
-    RawReferenceCompound, ReportSorter, check_sample_software_exists,
-    duplicate_metagenomics_software)
+    RawReferenceCompound,
+    ReportSorter,
+    check_sample_software_exists,
+    duplicate_metagenomics_software,
+)
 from settings.models import Parameter, Software
 
 
@@ -666,17 +678,14 @@ class SampleTableMetagenomics(SampleTable):
         row with two buttons, one for settings, one for deploy
         """
 
-
         return RunMain.objects.filter(
             sample__name=record.name,
             project=record.project,
-            parameter_set__status__in=[
-                ParameterSet.STATUS_FINISHED,
-            ],
-            run_type__in= [
+            run_type__in=[
                 RunMain.RUN_TYPE_MAP_REQUEST,
                 RunMain.RUN_TYPE_COMBINED_MAPPING,
-            ]
+            ],
+            status__in=[RunMain.STATUS_DEFAULT, RunMain.STATUS_RUNNING],
         ).count()
 
 

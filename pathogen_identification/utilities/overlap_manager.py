7176858ba9ea372cab8598e9014cf3ac29e084f8
SantosJGND <dourado.jns@gmail.com>
2024-02-12 13:14:36 +0000

    debug tree sort

--- pathogen_identification/utilities/overlap_manager.py
+++ pathogen_identification/utilities/overlap_manager.py
@@ -15,9 +15,9 @@ from scipy.spatial.distance import pdist, squareform
 
 from pathogen_identification.utilities.clade_objects import Clade, CladeFilter
 from pathogen_identification.utilities.phylo_tree import PhyloTreeManager
+
 ## pairwise matrix by individual reads
-from pathogen_identification.utilities.utilities_general import \
-    readname_from_fasta
+from pathogen_identification.utilities.utilities_general import readname_from_fasta
 
 
 def accid_from_metadata(metadata: pd.DataFrame, read_name: str) -> str:
@@ -272,6 +272,8 @@ class ReadOverlapManager:
         pairwise_shared_count = self.pairwise_shared_count(read_profile_matrix)
         print(pairwise_shared_count)
         pairwise_props = pairwise_shared_count / read_profile_matrix.sum(axis=0)
+
+        print(pairwise_props)
 
         return pairwise_props
 

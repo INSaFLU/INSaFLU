d66f23afc1fdd8fe789ada49627ff1cadcf9964f
SantosJGND <dourado.jns@gmail.com>
2024-02-13 12:01:25 +0000

    avoid copying read files when simlink

--- pathogen_identification/utilities/tree_deployment.py
+++ pathogen_identification/utilities/tree_deployment.py
@@ -14,22 +14,33 @@ from django.db.models import QuerySet
 from constants.constants import Televir_Metadata_Constants as Televir_Metadata
 from constants.constants import TypePath
 from fluwebvirus.settings import STATIC_ROOT
-from pathogen_identification.constants_settings import \
-    ConstantsSettings as PIConstants
-from pathogen_identification.models import (FinalReport, ParameterSet,
-                                            PIProject_Sample, Projects,
-                                            RunMain, SoftwareTree,
-                                            SoftwareTreeNode)
+from pathogen_identification.constants_settings import ConstantsSettings as PIConstants
+from pathogen_identification.models import (
+    FinalReport,
+    ParameterSet,
+    PIProject_Sample,
+    Projects,
+    RunMain,
+    SoftwareTree,
+    SoftwareTreeNode,
+)
 from pathogen_identification.modules.object_classes import Remap_Target
 from pathogen_identification.modules.remap_class import Mapping_Instance
 from pathogen_identification.modules.run_main import RunMainTree_class
-from pathogen_identification.utilities.televir_parameters import \
-    TelevirParameters
+from pathogen_identification.utilities.televir_parameters import TelevirParameters
 from pathogen_identification.utilities.update_DBs_tree import (
-    Update_Assembly, Update_Classification, Update_Remap,
-    Update_RunMain_Initial, Update_RunMain_Secondary, get_run_parents)
+    Update_Assembly,
+    Update_Classification,
+    Update_Remap,
+    Update_RunMain_Initial,
+    Update_RunMain_Secondary,
+    get_run_parents,
+)
 from pathogen_identification.utilities.utilities_pipeline import (
-    Pipeline_Makeup, PipelineTree, Utils_Manager)
+    Pipeline_Makeup,
+    PipelineTree,
+    Utils_Manager,
+)
 from pathogen_identification.utilities.utilities_views import ReportSorter
 from settings.constants_settings import ConstantsSettings
 from utils.utils import Utils
@@ -189,8 +200,7 @@ class PathogenIdentification_Deployment_Manager:
             shutil.rmtree(self.dir)
 
     def import_params(self, run_params_db: pd.DataFrame):
-        print("IMPORTING PARAMS")
-        print(run_params_db)
+
         self.run_params_db = run_params_db
 
     def run_main_prep_check_first(self):
@@ -291,9 +301,6 @@ class Tree_Node:
 
         self.parameters = self.determine_params(pipe_tree, sample)
         self.leaves = pipe_tree.leaves_from_node_using_graph(node_index)
-
-        print("NODE PARAMS")
-        print(self.parameters)
 
     def run_reference_overlap_analysis(self):
         # run = RunMain.objects.filter(parameter_set=self.parameter_set).first()
@@ -423,9 +430,6 @@ class Tree_Node:
         for node in self.branch:
             node_metadata = pipe_tree.node_index.loc[node].node
 
-            print("NODE METADATA")
-            print(pipe_tree.node_index)
-            print(pipe_tree.nodes)
             path_to_node = pipe_tree.paths_to_node(node)
             ps_visited = pipe_tree.check_if_leaf_steps_exist_list(
                 path_to_node, sample=sample
@@ -1362,8 +1366,6 @@ class TreeProgressGraph:
             tree.compress_tree()
             tree.split_modules()
             tree.get_module_tree()
-
-            print(f"tree leaves: {tree.leaves}")
 
             # network_df = [["NA", "0", "root", "input", "input", "lightblue"]]
             for edge in tree.edge_compress:

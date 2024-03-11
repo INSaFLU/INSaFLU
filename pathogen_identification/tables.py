9d544e60ea5fd3a662375b1cf6cc04c8beaaf18e
SantosJGND <dourado.jns@gmail.com>
2024-03-11 10:46:05 +0000

    panel mapping add main projecct table

--- pathogen_identification/tables.py
+++ pathogen_identification/tables.py
@@ -733,6 +733,7 @@ class SampleTableOne(tables.Table):
             run_type__in=[
                 RunMain.RUN_TYPE_MAP_REQUEST,
                 RunMain.RUN_TYPE_COMBINED_MAPPING,
+                RunMain.RUN_TYPE_PANEL_MAPPING,
             ],
             status__in=[
                 RunMain.STATUS_DEFAULT,

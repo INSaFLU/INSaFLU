1d4b98d92792ebe411621dfb4ec70e66343eb342
SantosJGND <dourado.jns@gmail.com>
2024-03-19 12:14:18 +0000

    set pipeline dendrograph to network script

--- pathogen_identification/utilities/tree_deployment.py
+++ pathogen_identification/utilities/tree_deployment.py
@@ -14,22 +14,33 @@ from django.db.models import QuerySet
 from constants.constants import Televir_Metadata_Constants as Televir_Metadata
 from constants.constants import TypePath
 from fluwebvirus.settings import STATIC_ROOT
-from pathogen_identification.constants_settings import \
-    ConstantsSettings as PIConstants
-from pathogen_identification.models import (FinalReport, ParameterSet,
-                                            PIProject_Sample, Projects,
-                                            RunMain, SoftwareTree,
-                                            SoftwareTreeNode)
+from pathogen_identification.constants_settings import ConstantsSettings as PIConstants
+from pathogen_identification.models import (
+    FinalReport,
+    ParameterSet,
+    PIProject_Sample,
+    Projects,
+    RunMain,
+    SoftwareTree,
+    SoftwareTreeNode,
+)
 from pathogen_identification.modules.object_classes import Remap_Target
 from pathogen_identification.modules.remap_class import Mapping_Instance
 from pathogen_identification.modules.run_main import RunMainTree_class
-from pathogen_identification.utilities.televir_parameters import \
-    TelevirParameters
+from pathogen_identification.utilities.televir_parameters import TelevirParameters
 from pathogen_identification.utilities.update_DBs_tree import (
-    Update_Assembly, Update_Classification, Update_Remap,
-    Update_RunMain_Initial, Update_RunMain_Secondary, get_run_parents)
+    Update_Assembly,
+    Update_Classification,
+    Update_Remap,
+    Update_RunMain_Initial,
+    Update_RunMain_Secondary,
+    get_run_parents,
+)
 from pathogen_identification.utilities.utilities_pipeline import (
-    Pipeline_Makeup, PipelineTree, Utils_Manager)
+    Pipeline_Makeup,
+    PipelineTree,
+    Utils_Manager,
+)
 from pathogen_identification.utilities.utilities_views import ReportSorter
 from settings.constants_settings import ConstantsSettings
 from utils.utils import Utils
@@ -1580,7 +1591,6 @@ class TreeProgressGraph:
     def generate_graph(self):
         stacked_df = self.get_combined_progress_df()
         print(stacked_df)
-        print(stacked_df)
         if stacked_df.shape[0] == 0:
             if os.path.exists(self.graph_html_path):
                 os.remove(self.graph_html_path)
@@ -1592,7 +1602,7 @@ class TreeProgressGraph:
             + "/bin/"
             + "Rscript",
             "--vanilla",
-            os.path.join(STATIC_ROOT, "R", "pipeline_dendrograph.R"),
+            os.path.join(STATIC_ROOT, "R", "pipeline_dendrograph_network.R"),
             self.stacked_df_path,
             self.graph_html_path,
             ",".join(stacked_df.columns).replace(" ", "."),

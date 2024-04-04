5a275775ec834fc283dda0df8d4730a5ddffe52d
SantosJGND <dourado.jns@gmail.com>
2024-04-04 16:48:29 +0000

    change colorscale heatmaps sample detail televir

--- static/js/televir_projects/heatmap_clades.js
+++ static/js/televir_projects/heatmap_clades.js
@@ -12,7 +12,18 @@ function createHeatmap(data, divId) {
         z: zValues,
         type: 'heatmap',
         hoverongaps: false,
-        colorscale: 'YlOrRd',
+        colorscale: [
+            ['0.0', 'rgb(49,54,149)'],
+            ['0.111111111111', 'rgb(69,117,180)'],
+            ['0.222222222222', 'rgb(116,173,209)'],
+            ['0.333333333333', 'rgb(171,217,233)'],
+            ['0.444444444444', 'rgb(224,243,248)'],
+            ['0.555555555556', 'rgb(254,224,144)'],
+            ['0.666666666667', 'rgb(253,174,97)'],
+            ['0.777777777778', 'rgb(244,109,67)'],
+            ['0.888888888889', 'rgb(215,48,39)'],
+            ['1.0', 'rgb(165,0,38)']
+        ],
         zmin: 0,  // Minimum color value
         zmax: 1,  // Maximum color value
     };
@@ -31,7 +42,7 @@ function createHeatmap(data, divId) {
 
     for ( var j = 0; j < xValues.length; j++ ) {
     var currentValue = zValues[j];
-    if (currentValue < 0.6) {
+    if (currentValue < 0.2 || currentValue > 0.8) {
         var textColor = 'white';
     }else{
         var textColor = 'black';

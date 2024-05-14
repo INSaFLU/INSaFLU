62fcd36bf0f85f76999ead1e91c213270a724fde
SantosJGND <dourado.jns@gmail.com>
2024-05-14 14:03:14 +0000

    lynt

--- pathogen_identification/utilities/utilities_pipeline.py
+++ pathogen_identification/utilities/utilities_pipeline.py
@@ -10,22 +10,19 @@ import pandas as pd
 from django.contrib.auth.models import User
 from django.db.models import Q, QuerySet
 
-from constants.constants import Televir_Directory_Constants as Televir_Directories
+from constants.constants import \
+    Televir_Directory_Constants as Televir_Directories
 from constants.constants import Televir_Metadata_Constants as Televir_Metadata
 from pathogen_identification.constants_settings import ConstantsSettings
 from pathogen_identification.host_library import Host
-from pathogen_identification.models import (
-    ParameterSet,
-    PIProject_Sample,
-    Projects,
-    RawReference,
-    RawReferenceCompoundModel,
-    RunMain,
-    SoftwareTree,
-    SoftwareTreeNode,
-)
-from pathogen_identification.utilities.utilities_televir_dbs import Utility_Repository
-from pathogen_identification.utilities.utilities_views import RawReferenceCompound
+from pathogen_identification.models import (ParameterSet, PIProject_Sample,
+                                            Projects, RawReference,
+                                            RawReferenceCompoundModel, RunMain,
+                                            SoftwareTree, SoftwareTreeNode)
+from pathogen_identification.utilities.utilities_televir_dbs import \
+    Utility_Repository
+from pathogen_identification.utilities.utilities_views import \
+    RawReferenceCompound
 from settings.constants_settings import ConstantsSettings as CS
 from settings.models import Parameter, PipelineStep, Software, Technology
 from utils.lock_atomic_transaction import LockedAtomicTransaction
@@ -553,6 +550,7 @@ class PipelineTree:
         all_paths = list(nx.all_simple_paths(self.graph, 0, self.leaves))
         all_paths = [self.get_path_explicit(path) for path in all_paths]
         path_dict = {path[-1][0]: path for x, path in enumerate(all_paths)}
+        print("#### GENERATED PATHS ####")
 
         return path_dict
 

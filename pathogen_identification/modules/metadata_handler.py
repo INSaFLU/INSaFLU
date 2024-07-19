444a325f79ca0219564bf8ae463a2cb587b32292
SantosJGND <dourado.jns@gmail.com>
2024-07-19 14:55:42 +0000

    debug generating remap target references

--- pathogen_identification/modules/metadata_handler.py
+++ pathogen_identification/modules/metadata_handler.py
@@ -6,16 +6,21 @@ from typing import List, Optional
 import pandas as pd
 
 from pathogen_identification.constants_settings import ConstantsSettings as CS
-from pathogen_identification.models import (PIProject_Sample, RawReference,
-                                            RawReferenceCompoundModel,
-                                            ReferenceSource,
-                                            ReferenceSourceFileMap, RunMain)
+from pathogen_identification.models import (
+    PIProject_Sample,
+    RawReference,
+    RawReferenceCompoundModel,
+    ReferenceSource,
+    ReferenceSourceFileMap,
+    RunMain,
+)
 from pathogen_identification.modules.object_classes import Remap_Target
 from pathogen_identification.utilities.entrez_wrapper import EntrezWrapper
-from pathogen_identification.utilities.utilities_general import (merge_classes,
-                                                                 simplify_name)
-from pathogen_identification.utilities.utilities_pipeline import \
-    RawReferenceUtils
+from pathogen_identification.utilities.utilities_general import (
+    merge_classes,
+    simplify_name,
+)
+from pathogen_identification.utilities.utilities_pipeline import RawReferenceUtils
 
 
 def determine_taxid_in_file(taxid, df: pd.DataFrame):
@@ -266,13 +271,14 @@ class RunMetadataHandler:
         max_remap: int = 15,
         taxid_limit: int = 12,
     ):
-        
+        print(report_1.shape)
+        print(report_2.shape)
         print("### processing reports")
         self.process_reports(
             report_1,
             report_2,
         )
-        
+
         print("### merging reports")
         if self.merged_targets.empty:
             self.merge_reports_clean(

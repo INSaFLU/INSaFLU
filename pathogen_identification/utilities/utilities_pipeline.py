116a516be0534a52235e0e72284cc95728b5a4e2
SantosJGND <dourado.jns@gmail.com>
2024-12-05 13:00:40 +0000

    remove pandas df append

--- pathogen_identification/utilities/utilities_pipeline.py
+++ pathogen_identification/utilities/utilities_pipeline.py
@@ -11,16 +11,18 @@ from django.contrib.auth.models import User
 from django.db import transaction
 from django.db.models import Q, QuerySet
 
-from constants.constants import \
-    Televir_Directory_Constants as Televir_Directories
+from constants.constants import Televir_Directory_Constants as Televir_Directories
 from constants.constants import Televir_Metadata_Constants as Televir_Metadata
 from pathogen_identification.constants_settings import ConstantsSettings
 from pathogen_identification.host_library import HomoSapiens, Host
-from pathogen_identification.models import (ParameterSet, PIProject_Sample,
-                                            Projects, SoftwareTree,
-                                            SoftwareTreeNode)
-from pathogen_identification.utilities.utilities_televir_dbs import \
-    Utility_Repository
+from pathogen_identification.models import (
+    ParameterSet,
+    PIProject_Sample,
+    Projects,
+    SoftwareTree,
+    SoftwareTreeNode,
+)
+from pathogen_identification.utilities.utilities_televir_dbs import Utility_Repository
 from settings.constants_settings import ConstantsSettings as CS
 from settings.models import Parameter, PipelineStep, Software, Technology
 from utils.lock_atomic_transaction import LockedAtomicTransaction
@@ -1232,9 +1234,9 @@ class Utility_Pipeline_Manager:
 
         try:
             with self.utility_repository.engine.connect() as conn:
-                r= conn.execute(fields)
-                rows= r.fetchall()
-                fields= pd.DataFrame(rows, columns=r.keys())
+                r = conn.execute(fields)
+                rows = r.fetchall()
+                fields = pd.DataFrame(rows, columns=r.keys())
                 fields = fields.drop_duplicates(subset=["database"])
 
             return fields
@@ -1346,9 +1348,10 @@ class Utility_Pipeline_Manager:
                     human_reference.host_name in host_df.host_name.unique()
                 ):  # place human dbs first
 
-                    host_df = host_df[
-                        host_df.host_name == human_reference.host_name
-                    ].append(host_df[host_df.host_name != human_reference.host_name])
+                    host_df = host_df[host_df.host_name == human_reference.host_name]
+                    host_df = pd.concat(
+                        [host_df, self.host_dbs[possibility].drop(host_df.index)]
+                    )
 
                 return list(
                     host_df[["path", "file_str"]].itertuples(index=False, name=None)

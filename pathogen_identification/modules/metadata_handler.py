2d29d5046a895f796e9e2d3b4934e8dbcb7b0910
SantosJGND <dourado.jns@gmail.com>
2024-12-17 11:43:26 +0000

    css; fve taxid fix

--- pathogen_identification/modules/metadata_handler.py
+++ pathogen_identification/modules/metadata_handler.py
@@ -6,20 +6,14 @@ from typing import List, Optional
 import pandas as pd
 
 from pathogen_identification.constants_settings import ConstantsSettings as CS
-from pathogen_identification.models import (
-    PIProject_Sample,
-    RawReference,
-    RawReferenceCompoundModel,
-    ReferenceSource,
-    ReferenceSourceFileMap,
-    RunMain,
-)
+from pathogen_identification.models import (PIProject_Sample, RawReference,
+                                            RawReferenceCompoundModel,
+                                            ReferenceSource,
+                                            ReferenceSourceFileMap, RunMain)
 from pathogen_identification.modules.object_classes import Remap_Target
 from pathogen_identification.utilities.entrez_wrapper import EntrezWrapper
-from pathogen_identification.utilities.utilities_general import (
-    merge_classes,
-    simplify_name,
-)
+from pathogen_identification.utilities.utilities_general import (merge_classes,
+                                                                 simplify_name)
 from pathogen_identification.utilities.utilities_views import RawReferenceUtils
 
 
@@ -381,13 +375,16 @@ class RunMetadataHandler:
     def filter_taxids_not_in_db(df) -> pd.DataFrame:
 
         def get_refs_existing(taxid):
-            return (
-                ReferenceSourceFileMap.objects.filter(
-                    reference_source__taxid__taxid=taxid,
+            try:
+                return (
+                    ReferenceSourceFileMap.objects.filter(
+                        reference_source__taxid__taxid=str(int(taxid)),
+                    )
+                    .distinct("reference_source__accid")
+                    .exists()
                 )
-                .distinct("reference_source__accid")
-                .exists()
-            )
+            except:
+                return False
 
         df["has_refs"] = df["taxid"].apply(get_refs_existing)
         df = df[df["has_refs"] == True]
@@ -541,9 +538,6 @@ class RunMetadataHandler:
                 else:
                     counts_df = df.groupby(["acc"]).size().reset_index(name="counts")
 
-                ### if acc in format ACC:ACC; get first acc
-                counts_df["acc"] = counts_df["acc"].apply(lambda x: x.split(":")[0])
-
                 df = self.merge_check_column_types(
                     counts_df,
                     self.accession_to_taxid,
@@ -558,6 +552,7 @@ class RunMetadataHandler:
 
         df = df[(df.taxid != "0") | (df.taxid != 0)]
 
+
         return df
 
     def db_get_taxid_descriptions(self, df: pd.DataFrame) -> pd.DataFrame:
@@ -568,7 +563,7 @@ class RunMetadataHandler:
         def get_description(taxid: str):
             try:
                 return (
-                    ReferenceSource.objects.filter(taxid__taxid=taxid)
+                    ReferenceSource.objects.filter(taxid__taxid=str(int(taxid)))
                     .first()
                     .description
                 )

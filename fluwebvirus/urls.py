45e5535f317872785180c412caa68c9e7f59c611
SantosJGND <dourado.jns@gmail.com>
2025-01-20 11:48:44 +0000

    update apps url files

--- fluwebvirus/urls.py
+++ fluwebvirus/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 
 
 from django.conf import settings
-from django.urls import include
-from django.conf.urls import re_path
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.urls import include, re_path
 
 from fluwebvirus.views import (
     ChangePasswordView,

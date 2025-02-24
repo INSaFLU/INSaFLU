8cba18a95c294c3849d99aa636a21516b1549c4b
SantosJGND <dourado.jns@gmail.com>
2025-02-24 11:46:48 +0000

    lynt generate default trees. Set software defaults on account creation.

--- pathogen_identification/management/commands/generate_default_trees.py
+++ pathogen_identification/management/commands/generate_default_trees.py
@@ -1,12 +1,7 @@
-import os
-from datetime import date
-
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 
-from pathogen_identification.utilities.utilities_pipeline import Utils_Manager
 from settings.default_software import DefaultSoftware
-from settings.models import Software
 
 
 class Command(BaseCommand):

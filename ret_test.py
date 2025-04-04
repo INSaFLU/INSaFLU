0e89d61fdc0215f79bd138e2a57ec7c8eb5a9468
SantosJGND <dourado.jns@gmail.com>
2025-04-04 10:44:31 +0000

    delete test_file

--- ret_test.py
+++
@@ -1,5 +0,0 @@
-from pathogen_identification.utilities.utilities_views import SampleReadsRetrieve
-from managing_files.models import Sample as INSaFLU_Sample
-sample = INSaFLU_Sample.objects.get(pk= 25)
-ret = SampleReadsRetrieve(sample)
-ret.sample_televir_paths()

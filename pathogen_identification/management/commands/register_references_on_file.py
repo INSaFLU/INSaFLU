83ac9c5cd90e9f5fb9646a7ae4f89141d7f828cb
SantosJGND <dourado.jns@gmail.com>
2025-07-14 13:05:18 +0000

    add televir reference register process process and process monitor info

--- pathogen_identification/management/commands/register_references_on_file.py
+++ pathogen_identification/management/commands/register_references_on_file.py
@@ -9,6 +9,7 @@ from django.core.management.base import BaseCommand
 from django.db.models import Q
 
 from constants.constants import Televir_Metadata_Constants
+from managing_files.models import ProcessControler
 from pathogen_identification.models import (
     ReferenceSource,
     ReferenceSourceFile,
@@ -16,6 +17,7 @@ from pathogen_identification.models import (
     ReferenceTaxid,
 )
 from pathogen_identification.utilities.entrez_wrapper import EntrezWrapper
+from utils.process_SGE import ProcessSGE
 from utils.utils import Utils
 
 
@@ -92,173 +94,214 @@ class Command(BaseCommand):
         # get user
         user = User.objects.get(pk=options["user_id"])
         utils: Utils = Utils()
-        outdir = utils.get_temp_dir()
-        os.makedirs(outdir, exist_ok=True)
-        metadadata_constants = Televir_Metadata_Constants()
+        process_controler = ProcessControler()
 
-        # entrez direct interface
-        entrez_connection = EntrezWrapper(
-            user.username,
-            bindir=metadadata_constants.get_software_bin_directory("entrez_direct"),
-            outdir=outdir,
-            outfile="entrez_output.tsv",
-            query_type="fetch_accession_description",
-            chunksize=500,
+        process_SGE = ProcessSGE()
+
+        ### SETUP
+        print(user.pk)
+        process_SGE.set_process_controlers(
+            user,
+            process_controler.get_name_televir_reference_update(user_pk=user.pk),
+            0,
         )
 
-        # accids on file
-        accid_file_path = metadadata_constants.accession_to_taxid_path
-        accid_file_df = pd.read_csv(accid_file_path, sep="\t")
+        process_SGE.set_process_controler(
+            user,
+            process_controler.get_name_televir_reference_update(
+                user_pk=user.pk,
+            ),
+            ProcessControler.FLAG_RUNNING,
+        )
 
-        print(f"Number of accids on file: {len(accid_file_df)}")
+        try:
+            outdir = utils.get_temp_dir()
+            os.makedirs(outdir, exist_ok=True)
+            metadadata_constants = Televir_Metadata_Constants()
 
-        files = accid_file_df.file.unique().tolist()
-        viros_file = [file for file in files if "virosaurus" in file]
-
-        if len(viros_file) == 0:
-            ignore_dict = {}
-        else:
-            viros_file = viros_file[0]
-
-            ignore_dict = extract_file_accids(
-                os.path.join(
-                    Televir_Metadata_Constants.SOURCE["REF_FASTA"],
-                    viros_file,
-                ),
-                os.path.join(outdir, "ignore_accids.txt"),
-                "GENE",
+            # entrez direct interface
+            entrez_connection = EntrezWrapper(
+                user.username,
+                bindir=metadadata_constants.get_software_bin_directory("entrez_direct"),
+                outdir=outdir,
+                outfile="entrez_output.tsv",
+                query_type="fetch_accession_description",
+                chunksize=500,
             )
 
-        if options["curate"] is False:
-            entrez_descriptions = []
-            for file_source, file_df in accid_file_df.groupby("file"):
+            # accids on file
+            accid_file_path = metadadata_constants.accession_to_taxid_path
+            accid_file_df = pd.read_csv(accid_file_path, sep="\t")
 
-                file_descriptor = entrez_connection.run_entrez_query(
-                    query_list=file_df.acc.unique().tolist(),
+            print(f"Number of accids on file: {len(accid_file_df)}")
+
+            files = accid_file_df.file.unique().tolist()
+            viros_file = [file for file in files if "virosaurus" in file]
+
+            if len(viros_file) == 0:
+                ignore_dict = {}
+            else:
+                viros_file = viros_file[0]
+
+                ignore_dict = extract_file_accids(
+                    os.path.join(
+                        Televir_Metadata_Constants.SOURCE["REF_FASTA"],
+                        viros_file,
+                    ),
+                    os.path.join(outdir, "ignore_accids.txt"),
+                    "GENE",
                 )
-                file_descriptor["file"] = file_source
-                entrez_descriptions.append(file_descriptor)
 
-            entrez_descriptions = pd.concat(entrez_descriptions, ignore_index=True)
+            if options["curate"] is False:
+                entrez_descriptions = []
+                for file_source, file_df in accid_file_df.groupby("file"):
 
-        else:
-            entrez_descriptions = ReferenceSource.objects.all()
-            entrez_descriptions = pd.DataFrame(
-                [
-                    {
-                        "accession": source.accid,
-                        "description": source.description,
-                        "taxid": source.taxid.taxid,
-                    }
-                    for source in entrez_descriptions
-                ]
-            )
+                    file_descriptor = entrez_connection.run_entrez_query(
+                        query_list=file_df.acc.unique().tolist(),
+                    )
+                    file_descriptor["file"] = file_source
+                    entrez_descriptions.append(file_descriptor)
 
-        print("Retrieved entrez descriptions")
-        print(f"Number of entrez descriptions: {len(entrez_descriptions)}")
-        print("Registering entrez descriptions")
+                entrez_descriptions = pd.concat(entrez_descriptions, ignore_index=True)
 
-        d = 0
+            else:
+                entrez_descriptions = ReferenceSource.objects.all()
+                entrez_descriptions = pd.DataFrame(
+                    [
+                        {
+                            "accession": source.accid,
+                            "description": source.description,
+                            "taxid": source.taxid.taxid,
+                        }
+                        for source in entrez_descriptions
+                    ]
+                )
 
-        # raise Exception("Debugging point reached")
+            print("Retrieved entrez descriptions")
+            print(f"Number of entrez descriptions: {len(entrez_descriptions)}")
+            print("Registering entrez descriptions")
 
-        for taxid_str, taxid_df in entrez_descriptions.groupby("taxid"):
-            if pd.isna(taxid_str):
-                print("Skipping NaN taxid")
-                continue
-            taxid_str = str(int(taxid_str))
+            d = 0
 
-            try:
-                ref_taxid = ReferenceTaxid.objects.get(taxid=taxid_str)
-            except ReferenceTaxid.DoesNotExist:
-                ref_taxid = ReferenceTaxid.objects.create(taxid=taxid_str)
+            # raise Exception("Debugging point reached")
 
-            for _, row in taxid_df.iterrows():
-                if pd.isna(row.accession):
-                    print("Skipping NaN accession")
+            for taxid_str, taxid_df in entrez_descriptions.groupby("taxid"):
+                if pd.isna(taxid_str):
+                    print("Skipping NaN taxid")
                     continue
+                taxid_str = str(int(taxid_str))
 
-                if pd.isna(row.description):
-                    print("Skipping NaN description")
-                    continue
+                try:
+                    ref_taxid = ReferenceTaxid.objects.get(taxid=taxid_str)
+                except ReferenceTaxid.DoesNotExist:
+                    ref_taxid = ReferenceTaxid.objects.create(taxid=taxid_str)
 
-                if pd.isna(row.file):
-                    print("Skipping NaN file")
-                    continue
+                for _, row in taxid_df.iterrows():
+                    if pd.isna(row.accession):
+                        print("Skipping NaN accession")
+                        continue
 
-                ### register a log every 1000 accids
-                d += 1
+                    if pd.isna(row.description):
+                        print("Skipping NaN description")
+                        continue
 
-                if d % 1000 == 0:
-                    print(f"Taxid: {taxid_str}")
-                    print(f"Number of taxids processed: {d}")
+                    if pd.isna(row.file):
+                        print("Skipping NaN file")
+                        continue
 
-                accid_str = row.accession
-                simple_accid = accid_str.split(".")[0]
+                    ### register a log every 1000 accids
+                    d += 1
 
-                description = row.description
+                    if d % 1000 == 0:
+                        print(f"Taxid: {taxid_str}")
+                        print(f"Number of taxids processed: {d}")
 
-                if len(description) > 300:
-                    description = description[:300]
+                    accid_str = row.accession
+                    simple_accid = accid_str.split(".")[0]
 
-                file_str = row.file
+                    description = row.description
 
-                if (
-                    ignore_dict.get(simple_accid, None) is not None
-                    and options["curate"] is False
-                    and "viros" in file_str
-                ):
+                    if len(description) > 300:
+                        description = description[:300]
+
+                    file_str = row.file
+
+                    if (
+                        ignore_dict.get(simple_accid, None) is not None
+                        and options["curate"] is False
+                        and "viros" in file_str
+                    ):
+
+                        ref_source = ReferenceSource.objects.filter(accid=accid_str)
+
+                        viro_maps = ReferenceSourceFileMap.objects.filter(
+                            reference_source__accid=accid_str,
+                            reference_source_file__file=file_str,
+                        )
+                        viro_maps.delete()
+
+                        any_left = ReferenceSourceFileMap.objects.filter(
+                            reference_source__accid=accid_str,
+                        )
+
+                        if ref_source and not any_left.exists():
+                            ref_source.delete()
+
+                        continue
 
                     ref_source = ReferenceSource.objects.filter(accid=accid_str)
 
-                    viro_maps = ReferenceSourceFileMap.objects.filter(
-                        reference_source__accid=accid_str,
-                        reference_source_file__file=file_str,
-                    )
-                    viro_maps.delete()
+                    if ref_source.exists() is False:
+                        ref_source = ReferenceSource.objects.create(
+                            accid=accid_str, description=description, taxid=ref_taxid
+                        )
 
-                    any_left = ReferenceSourceFileMap.objects.filter(
-                        reference_source__accid=accid_str,
-                    )
+                    elif ref_source.count() > 1:
 
-                    if ref_source and not any_left.exists():
                         ref_source.delete()
+                        ref_source = ReferenceSource.objects.create(
+                            accid=accid_str, description=description, taxid=ref_taxid
+                        )
 
-                    continue
+                    else:
+                        ref_source = ref_source.first()
 
-                ref_source = ReferenceSource.objects.filter(accid=accid_str)
+                    # get reference source file
+                    try:
+                        ref_source_file = ReferenceSourceFile.objects.get(file=file_str)
+                    except ReferenceSourceFile.DoesNotExist:
+                        ref_source_file = ReferenceSourceFile.objects.create(
+                            file=file_str
+                        )
 
-                if ref_source.exists() is False:
-                    ref_source = ReferenceSource.objects.create(
-                        accid=accid_str, description=description, taxid=ref_taxid
-                    )
+                    # description = entrez_connection
 
-                elif ref_source.count() > 1:
+                    try:
+                        _ = ReferenceSourceFileMap.objects.get(
+                            reference_source=ref_source,
+                            reference_source_file=ref_source_file,
+                        )
 
-                    ref_source.delete()
-                    ref_source = ReferenceSource.objects.create(
-                        accid=accid_str, description=description, taxid=ref_taxid
-                    )
+                    except ReferenceSourceFileMap.DoesNotExist:
+                        _ = ReferenceSourceFileMap.objects.create(
+                            reference_source=ref_source,
+                            reference_source_file=ref_source_file,
+                        )
+        except Exception as e:
+            print(e)
+            process_SGE.set_process_controler(
+                user,
+                process_controler.get_name_televir_reference_update(
+                    user_pk=user.pk,
+                ),
+                ProcessControler.FLAG_ERROR,
+            )
+            return
 
-                else:
-                    ref_source = ref_source.first()
-
-                # get reference source file
-                try:
-                    ref_source_file = ReferenceSourceFile.objects.get(file=file_str)
-                except ReferenceSourceFile.DoesNotExist:
-                    ref_source_file = ReferenceSourceFile.objects.create(file=file_str)
-
-                # description = entrez_connection
-
-                try:
-                    _ = ReferenceSourceFileMap.objects.get(
-                        reference_source=ref_source,
-                        reference_source_file=ref_source_file,
-                    )
-
-                except ReferenceSourceFileMap.DoesNotExist:
-                    _ = ReferenceSourceFileMap.objects.create(
-                        reference_source=ref_source,
-                        reference_source_file=ref_source_file,
-                    )
+        process_SGE.set_process_controler(
+            user,
+            process_controler.get_name_televir_reference_update(
+                user_pk=user.pk,
+            ),
+            ProcessControler.FLAG_FINISHED,
+        )

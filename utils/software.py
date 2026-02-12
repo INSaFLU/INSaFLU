bda3626ad502c4a35a868a8c0017376d42179dc2
dsobral <daniel.sobral@insa.min-saude.pt>
2026-02-12 15:35:00 +0000

    [add] Remove temporary folder

--- utils/software.py
+++ utils/software.py
@@ -6028,7 +6028,7 @@ class Software(object):
             alignment_file,
         )
 
-        #self.utils.remove_dir(temp_dir)
+        self.utils.remove_dir(temp_dir)
 
         return [tree_file, alignment_file, auspice_zip]
 

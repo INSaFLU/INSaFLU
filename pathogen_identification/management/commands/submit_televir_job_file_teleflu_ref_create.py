7d7d9b94bef9877eddb7f42c11bcc9cced7305be
SantosJGND <dourado.jns@gmail.com>
2026-02-24 16:30:58 +0000

    [televir] - fix reference tables file import and debug refernce creation, re-introduce mappings worklflows tables for main sample page

--- pathogen_identification/management/commands/submit_televir_job_file_teleflu_ref_create.py
+++ pathogen_identification/management/commands/submit_televir_job_file_teleflu_ref_create.py
@@ -64,8 +64,7 @@ class Command(BaseCommand):
 
         else:
             print("Process does not exist")
-
-        # UTILITIES
+            
 
         try:
             success, ref_id = file_reference_to_insaflu(ref_id, user_id)

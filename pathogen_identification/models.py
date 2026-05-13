33fd23cc8982ddee51c5880081299503545cf102
SantosJGND <dourado.jns@gmail.com>
2026-05-13 15:34:06 +0000

    reset migrations; fix tax fields and related_names in pathogen identification ReferenceTaxid model

--- pathogen_identification/models.py
+++ pathogen_identification/models.py
@@ -1608,12 +1608,12 @@ class TelefluMappedSample(models.Model):
 class ReferenceTaxid(models.Model):
     taxid = models.CharField(max_length=100, blank=True, null=True)
 
-    genus = models.ForeignKey(Taxon, on_delete=models.CASCADE, blank=True, null=True)
-    family = models.ForeignKey(Taxon, on_delete=models.CASCADE, blank=True, null=True)
-    order = models.ForeignKey(Taxon, on_delete=models.CASCADE, blank=True, null=True)
-    class_ = models.ForeignKey(Taxon, on_delete=models.CASCADE, blank=True, null=True)
-    phylum = models.ForeignKey(Taxon, on_delete=models.CASCADE, blank=True, null=True)
-    domain = models.ForeignKey(Taxon, on_delete=models.CASCADE, blank=True, null=True) 
+    tax_genus = models.ForeignKey(Taxon, on_delete=models.CASCADE, blank=True, null=True, related_name="tax_genus")
+    tax_family = models.ForeignKey(Taxon, on_delete=models.CASCADE, blank=True, null=True, related_name="tax_family")
+    tax_order = models.ForeignKey(Taxon, on_delete=models.CASCADE, blank=True, null=True, related_name="tax_order")
+    tax_class = models.ForeignKey(Taxon, on_delete=models.CASCADE, blank=True, null=True, related_name="tax_class")
+    tax_phylum = models.ForeignKey(Taxon, on_delete=models.CASCADE, blank=True, null=True, related_name="tax_phylum")
+    tax_domain = models.ForeignKey(Taxon, on_delete=models.CASCADE, blank=True, null=True, related_name="tax_domain")
 
     def __str__(self):
         return self.taxid
